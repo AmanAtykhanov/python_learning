bf05b632409f4aa56bdf701efc167eb9676cebca
AmanAtykhanov <aman.atykhanov@mail.ru>
2020-09-24 10:44:24 +0600

    Update class hierarchy.py

--- class hierarchy.py
+++ class hierarchy.py
@@ -1,3 +1,4 @@
+#файловое чтение
 # class TreeClass:
 #     def __init__(self, arg1):
 #         self.tree_classes = dict()
@@ -78,13 +79,12 @@
 #         except KeyError:
 #             return
 #
-# #f = open("cmds.txt", "r")
+# f = open("cmds.txt", "r")
 #
-# count_class = int(input())
-# #count_class = int(f.readline())
+# count_class = int(f.readline())
 # obj_tree_class = TreeClass(count_class)
 # for _ in range(count_class):
-#     cmd_add_class = input()#f.readline()
+#     cmd_add_class = f.readline()
 #     if len(cmd_add_class) > 3:
 #         cmd_add_class = cmd_add_class.split(':')
 #         children_name = cmd_add_class[0][0]
@@ -94,40 +94,39 @@
 #     else:
 #         obj_tree_class.create(cmd_add_class.rstrip('\n'))
 #
-# #count_request = int(f.readline())
-# count_request = int(input())
+# count_request = int(f.readline())
 # for _ in range(count_request):
-#     #parent_class_name, children_class_name = (f.readline()).split()
-#     parent_class_name, children_class_name = input().split()
+#     parent_class_name, children_class_name = (f.readline()).split(
 #     print(obj_tree_class.get_information(parent_class_name, children_class_name))
-# #f.close()
+# f.close()
 
 class TreeClass :
-    def __init__ ( self , arg1 ) :
-        self.tree_classes = dict ( )
-        self.count_classes = arg1
+    def __init__ ( self , arg_count_classes ) :
+        self.tree_classes = dict()
+        self.count_classes = arg_count_classes
         self.answer = ''
 
     def create ( self , children_name ) :
-        self.tree_classes['children ' + children_name] = set ( )
+        self.tree_classes['children ' + children_name] = set ( ) #добавляем новый класс с пустым списком родителей
 
     def put_class ( self , children_name_class , ls_parents_class=[] ) :
-        self.tree_classes['children ' + children_name_class].update ( ls_parents_class )
+        self.tree_classes['children ' + children_name_class].update ( ls_parents_class ) #добавляем новый класс с списком родителей ls_parents_class 
 
     def get_information ( self , parent_class_name , children_class_name ) :
-        self.answer = 'No'
-        self.find_realation ( parent_class_name , children_class_name )
-        return self.answer
+        self.answer = 'No' #изначально предполагаются что классы передаваемые через атрибуты функции не связаны
+        self.find_realation ( parent_class_name , children_class_name ) #функция поиска связи между классами
+        return self.answer #возвращаем ответ
 
     def find_realation ( self , parent_class_name , children_class_name ) :
+        #готовимся поймать исключение
         try :
-            ls_parents = self.tree_classes['children ' + children_class_name]
-            if parent_class_name == children_class_name or parent_class_name in ls_parents :
-                self.answer = 'Yes'
+            ls_parents = self.tree_classes['children ' + children_class_name] #вытаскиваем значение(список родителей) по ключу (имени дочернего класса)
+            if parent_class_name == children_class_name or parent_class_name in ls_parents : #первое условие для случая A A, второе если в списке родителей есть наш родитель
+                self.answer = 'Yes' #меняем на да, теперь выходим из рекурсии
                 return
-            for parent in ls_parents :
-                self.find_realation ( parent_class_name , parent )
-                if self.answer == 'Yes' :
+            for parent in ls_parents : #проходим по каждому из родителей
+                self.find_realation ( parent_class_name , parent ) #начинается рекурсия
+                if self.answer == 'Yes' : # если в списке родителей нашли родителя то выходим из рекурсии
                     return
         except :
             return
@@ -138,15 +137,33 @@ obj_tree_class = TreeClass ( count_class )
 for _ in range ( count_class ) :
     cmd_add_class = input ( )
     if len ( cmd_add_class ) > 3 :
-        cmd_add_class = cmd_add_class.split ( ':' )
-        children_name = cmd_add_class[0][0]
-        parents_name = cmd_add_class[1].split ( )
-        obj_tree_class.create ( children_name )
-        obj_tree_class.put_class ( children_name , parents_name )
+        #на случай обработки строки вида A(children) : B C D S R G(parents)
+        #ещё примеры
+        #G : A
+        #A : A
+        #G : A C V
+        #G : A Q R T B C
+        #T : F F
+        #G : A Q
+        #G : A
+        #G : A
+        cmd_add_class = cmd_add_class.split ( ':' ) #режим строку по :
+        children_name = cmd_add_class[0][0] # берём имя ребёнка
+        parents_name = cmd_add_class[1].split ( ) # берём имена родителей
+        obj_tree_class.create ( children_name ) #создаём вершину с именем ребёнка
+        obj_tree_class.put_class ( children_name , parents_name ) #добавляем в словарь ключ значение
     else :
-        obj_tree_class.create ( cmd_add_class.rstrip ( '\n' ) )
+        #на случай обработки строки вида A
+        #ещё примеры
+        #E
+        #D
+        #I
+        #O
+        #P
+        #L
+        obj_tree_class.create ( cmd_add_class.rstrip ( '\n' ) ) #создаём вершину с пустым список значений
 
-count_request = int ( input ( ) )
+count_request = int ( input ( ) ) #количетсво запросов
 for _ in range ( count_request ) :
-    parent_class_name , children_class_name = input ( ).split ( )
-    print ( obj_tree_class.get_information ( parent_class_name , children_class_name ) )
+    parent_class_name , children_class_name = input ( ).split ( )#делим строку находим родителя и ребёнка
+    print ( obj_tree_class.get_information ( parent_class_name , children_class_name ) ) #ищем инфомрацию об отношении между parent_class_name и children_class_name, печатаем ответ
