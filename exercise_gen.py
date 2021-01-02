98d652e695cd79915f5324b94344590ad1091d6e
AmanAtykhanov <aman.atykhanov@mail.ru>
2021-01-02 13:19:02 +0600

    Update exercise_gen.py

--- exercise_gen.py
+++ exercise_gen.py
@@ -1,12 +1,18 @@
 from itertools import takewhile
 import itertools
 
+def is_prostoe(num):
+    for i in range(2, num - 1):
+        if num % i == 0:
+            return False
+    return True
+
 def primes():
     a = 2
     yield a
     while True:
         a += 1
-        if a % 2 == 1:
+        if is_prostoe(a):
             yield a
 
 ge = primes()
